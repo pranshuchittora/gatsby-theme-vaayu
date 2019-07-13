9efc6b394ecbcb431e8632896213b6e7fb5f1101
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-07-13 13:50:25 +0530

    chore(npm): just published the theme !!!!yay!!!!

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Theme Vaayu",
   },
-  plugins: ["gatsby-plugin-theme-ui"],
+  plugins: ["gatsby-plugin-theme-ui","gatsby-plugin-react-helmet"],
 }
--- theme/src/templates/page.js
+++ theme/src/templates/page.js
@@ -1,9 +1,18 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import { Styled } from "theme-ui"
 import Layout from "../components/layout"
 
 const PageTemplate = ({ pageContext }) => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <meta
+        name="Gatsby theme vaayu"
+        content="Gatsby theme vaayu made by Pranshu Chittora for Gatsby Theme Jam"
+      />
+      <title>Gatsby Theme Vaayu</title>
+    </Helmet>
     <Styled.h1>{pageContext.heading}</Styled.h1>
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
   </Layout>
